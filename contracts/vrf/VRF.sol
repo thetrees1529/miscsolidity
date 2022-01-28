a9fd70c77d9045ab1d8b139cd49ad02edbe1d095
Luke Hannigan <lukeyhannigany@gmail.com>
2022-01-28 17:53:14 +0000

    bruh

--- contracts/vrf/VRF.sol
+++ contracts/vrf/VRF.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract VRF {
-    function _vrf() internal view returns (bytes32 result) {
+    function _vrf(uint blockNumber) internal view returns (bytes32 result) {
         uint[1] memory bn;
-        bn[0] = block.number;
+        bn[0] = blockNumber;
         assembly {
             let memPtr := mload(0x40)
             if iszero(staticcall(not(0), 0xff, bn, 0x20, memPtr, 0x20)) {
