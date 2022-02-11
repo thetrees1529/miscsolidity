44f163af7c12c8da7d98641530ba571993745b6c
Luke Hannigan <lukeyhannigany@gmail.com>
2022-02-11 19:08:43 +0000

    misc

--- contracts/access/OwnableAccessControl.sol
+++ contracts/access/OwnableAccessControl.sol
@@ -2,7 +2,7 @@
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 pragma solidity ^0.8.0;
-contract OwnerRoleEditor is Ownable, AccessControl {
+contract OwnableAccessControl is Ownable, AccessControl {
     function grantRole(bytes32 role, address account) public override {
         msg.sender == owner() ? _grantRole(role, account) : super.grantRole(role, account);
     }
