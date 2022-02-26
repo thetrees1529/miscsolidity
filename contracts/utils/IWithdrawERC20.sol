0610a4b814d5bf0834a460fd5c52b43ba60b0849
Luke Hannigan <lukeyhannigany@gmail.com>
2022-02-26 14:23:03 +0000

    withdrawerc20

--- contracts/utils/IWithdrawERC20.sol
+++ contracts/utils/IWithdrawERC20.sol
@@ -2,7 +2,7 @@
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 pragma solidity ^0.8.0;
 
-interface IWithdraw {
+interface IWithdrawERC20 {
     function WITHDRAW_ERC20_ROLE() external view returns(bytes32);
     function withdrawERC20(IERC20 token, address to, uint value) external;
 }
--- contracts/utils/WithdrawERC20.sol
+++ contracts/utils/WithdrawERC20.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 import "./IWithdrawERC20.sol";
 
-contract Withdraw is IWithdraw, AccessControl {
+contract WithdrawERC20 is IWithdrawERC20, AccessControl {
     bytes32 public override WITHDRAW_ERC20_ROLE = keccak256("WITHDRAW_ERC20_ROLE");
     function withdrawERC20(IERC20 token, address to, uint value) public override onlyRole(WITHDRAW_ERC20_ROLE) {
         token.transfer(to, value);
